69b96ad263ad3b9072d0a0aa12cc23f82565f6e1
zoomdong <1344492820@qq.com>
2018-08-17 23:52:35 +0800

    完成了底部信息栏组件

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+// import IndexPage from './routes/IndexPage';
+import Appbottom from './routes/Appbottom/Appbottom';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={Appbottom} />
       </Switch>
     </Router>
   );
