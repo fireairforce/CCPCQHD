524cc136e152b5b8a90ca2d442e38321ea23d5e3
YYL1999 <2291003927@qq.com>
2018-08-28 10:26:12 +0800

    路由搭建

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import dynamic from 'dva/dynamic'
+
+import { Router, Route, Switch } from 'dva/router';
+// import IndexPage from './routes/IndexPage';
+
+
+
+function RouterConfig({ history,app }) {
+  const App = dynamic({
+    app,
+    component: () => import('./routes/index')
+  })
+  const Login = dynamic({
+    app,
+    component: () => import('./routes/Login/Login')
+  })
+  const Manage =dynamic({
+    app,
+    component:()=>import('./routes/manage/index')
+  })
+  return (
+    <Router history={history}>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/login"  component={Login} />
+                <Route path='/manage' component={Manage} />
+            </Switch>
+        </Router>
+  
+
+
+  );
+}
+
+export default RouterConfig;
---
+++ src/routes/manage/header.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Menu, Icon, Layout } from 'antd'
+import { Link } from 'react-router-dom'
+// import * as screenfull from 'screenfull'
+import styles from'./header.less'
+import 'antd/dist/antd.css'
+
+const SubMenu = Menu.SubMenu
+const { Header } = Layout
+
+export default class Top extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getUser()
+    }
+
+    getUser = () => {
+        this.setState({
+            username: 'Lpf'
+        })
+    }
+
+    clear = (item) => {
+        if (item.key === 'logOut') {
+            this.props.clear()
+        }
+    }
+
+    // screenFull = () => {
+    //     if (screenfull.enabled) {
+    //         screenfull.request();
+    //     }
+    // }
+    render() {
+        return (
+            <Header style={{ background: '#fff'}}>
+                <Icon
+                    className={styles.trigger}
+                    type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
+                    onClick={this.props.toggle}
+                />
+                <Menu mode="horizontal" className={styles.logOut} onClick={this.clear}>
+                    <SubMenu title={<span><Icon type="user" />{ this.state.username }</span>} >
+                        <Menu.Item key="logOut"><Link to="/login" >退出</Link></Menu.Item>
+                    </SubMenu>
+                </Menu>
+                {/* <Icon
+                    className="screenFull"
+                    type="arrows-alt"
+                    onClick={this.screenFull}
+                /> */}
+            </Header>
+        )
+    }
+}
